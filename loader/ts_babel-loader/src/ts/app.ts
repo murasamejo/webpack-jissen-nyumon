93d40179e30daa042e3d9b7e10738f34b895f8e6
corselia <takiya@toran.sakura.ne.jp>
2020-01-31 23:33:40 +0900

    TypeScript で Puppeteer のコードを書いた

--- loader/ts_babel-loader/src/ts/app.ts
+++ loader/ts_babel-loader/src/ts/app.ts
@@ -1,2 +1,13 @@
-const greeting: string = 'Hello, TypeScript world!!';
-console.log(greeting);
+import puppeteer from 'puppeteer';
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  const url: string = 'https://www.google.co.jp/';
+  const screenshot_filename: string = 'puppeteer_screenshot.png';
+  await page.goto(url);
+  await page.screenshot({ path: screenshot_filename });
+
+  await browser.close();
+})();
